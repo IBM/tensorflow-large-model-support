082ee751e618b48fda5724485854877e6d15f38d
Tung Le Duc <tung@jp.ibm.com>
2018-06-19 14:29:20 +0900

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name='tflms',
-    version='0.2.0',
+    version='0.3.0',
     description='tflms: graph editing library for large model support',
     author='Tung D. Le',
     author_email='tung@jp.ibm.com',
