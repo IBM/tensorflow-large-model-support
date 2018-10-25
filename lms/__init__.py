2cde32d11085fd4caddc696390bffe2c56851095
Samuel Matzek <smatzek@us.ibm.com>
2018-10-25 14:50:30 -0500

    Remove unneeded import in __init__.py

--- lms/__init__.py
+++ lms/__init__.py
@@ -1,7 +1,3 @@
 from lms.lms import LMS
 from lms.lms import LMSSessionRunHook
 from lms.lms import LMSKerasCallback
-
-# pylint: disable=wildcard-import
-from lms.util import *
-# pylint: enable=wildcard-import
