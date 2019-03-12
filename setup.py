07491811b80b5becc74bf1f58adaf6c187fbf018
TUNG LEDUC <TUNG@jp.ibm.com>
2019-03-12 17:05:02 +0900

    Update setup.py
    To match with TFLMSv2 in PowerAI

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup(
     name='tflms',
-    version='0.4.0',
+    version='2.0.0',
     description='tflms: graph editing library for large model support',
     author='Tung D. Le',
     author_email='tung@jp.ibm.com',
